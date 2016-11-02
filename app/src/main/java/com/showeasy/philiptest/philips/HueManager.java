bf6b307734854ee75311db7bcde59a8aecdafc9c
syz2580 <shaoyizhe_2006@sina.com>
2016-11-02 10:26:23 +0800

    连接模块拆分出来

--- app/src/main/java/com/showeasy/philiptest/philips/HueManager.java
+++ app/src/main/java/com/showeasy/philiptest/philips/HueManager.java
@@ -26,7 +26,8 @@ import java.util.List;
 public class HueManager implements IHue {
 
     private PHHueSDK phHueSDK;
-    PHBridgeResourcesCache cache;
+    private HueBridgeConnection connection;
+    private PHBridgeResourcesCache cache;
 
     private static class HueManagerHolder {
         private static final HueManager instance = new HueManager();
@@ -39,26 +40,22 @@ public class HueManager implements IHue {
         phHueSDK = PHHueSDK.create();
         phHueSDK.setAppName("PhilipTest");
         phHueSDK.setDeviceName(Build.MODEL);
-        phHueSDK.getNotificationManager().registerSDKListener(mListener);
+        connection = new HueBridgeConnection(phHueSDK);
     }
 
     @Override
     public void searchBridge() {
-        PHBridgeSearchManager sm = (PHBridgeSearchManager) phHueSDK.getSDKService(PHHueSDK.SEARCH_BRIDGE);
-        sm.search(true, true);
+        connection.searchBridge();
     }
 
     @Override
     public void connectBridge(String ip, String username) {
-        PHAccessPoint accessPoint = new PHAccessPoint();
-        accessPoint.setIpAddress(ip);
-        accessPoint.setUsername(username);
-        phHueSDK.connect(accessPoint);
+        connection.connectBridge(ip,username);
     }
 
     @Override
     public void disconnectAll() {
-        phHueSDK.disableAllHeartbeat();
+        connection.disconnectAll();
     }
 
     @Override
@@ -107,63 +104,5 @@ public class HueManager implements IHue {
         // TODO: 2016/11/1
         callback.onNotify(result);
     }
-
-    private PHSDKListener mListener = new PHSDKListener() {
-        @Override
-        public void onAccessPointsFound(List<PHAccessPoint> accessPoint) {
-            // Handle your bridge search results here.  Typically if multiple results are returned you will want to display them in a list
-            // and let the user select their bridge.   If one is found you may opt to connect automatically to that bridge.
-        }
-
-        @Override
-        public void onCacheUpdated(List<Integer> cacheNotificationsList, PHBridge bridge) {
-            // Here you receive notifications that the BridgeResource Cache was updated. Use the PHMessageType to
-            // check which cache was updated, e.g.
-            if (cacheNotificationsList.contains(PHMessageType.LIGHTS_CACHE_UPDATED)) {
-                System.out.println("Lights Cache Updated ");
-            }
-        }
-
-        @Override
-        public void onBridgeConnected(PHBridge b, String username) {
-            phHueSDK.setSelectedBridge(b);
-            phHueSDK.enableHeartbeat(b, PHHueSDK.HB_INTERVAL);
-            phHueSDK.getLastHeartbeat().put(b.getResourceCache().getBridgeConfiguration() .getIpAddress(), System.currentTimeMillis());
-            SharedPrefsManager.getInstance().setLastConnectedIPAddress(b.getResourceCache().getBridgeConfiguration().getIpAddress());
-            SharedPrefsManager.getInstance().setLastConnectedUsername(username);
-            // TODO: 2016/11/1 Notify UI 
-            // Here it is recommended to set your connected bridge in your sdk object (as above) and start the heartbeat.
-            // At this point you are connected to a bridge so you should pass control to your main program/activity.
-            // The username is generated randomly by the bridge.
-            // Also it is recommended you store the connected IP Address/ Username in your app here.  This will allow easy automatic connection on subsequent use.
-        }
-
-        @Override
-        public void onAuthenticationRequired(PHAccessPoint accessPoint) {
-            phHueSDK.startPushlinkAuthentication(accessPoint);
-            // Arriving here indicates that Pushlinking is required (to prove the User has physical access to the bridge).  Typically here
-            // you will display a pushlink image (with a timer) indicating to to the user they need to push the button on their bridge within 30 seconds.
-        }
-
-        @Override
-        public void onConnectionResumed(PHBridge bridge) {
-
-        }
-
-        @Override
-        public void onConnectionLost(PHAccessPoint accessPoint) {
-            // Here you would handle the loss of connection to your bridge.
-        }
-
-        @Override
-        public void onError(int code, final String message) {
-            // Here you can handle events such as Bridge Not Responding, Authentication Failed and Bridge Not Found
-        }
-
-        @Override
-        public void onParsingErrors(List<PHHueParsingError> parsingErrorsList) {
-            // Any JSON parsing errors are returned here.  Typically your program should never return these.
-        }
-    };
 
 }
